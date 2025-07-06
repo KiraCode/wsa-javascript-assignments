afa76c777dec61bc920b697480fa88c5acea69b0
no name <your-email@example.com>
2025-07-07 00:10:51 +0530

    assignment 4 is added

--- src/assets/data/assignment.js
+++ src/assets/data/assignment.js
@@ -24,11 +24,16 @@ const details = [
       "Write a JavaScript program to convert the given temperature in Fahrenheit to Celsius using the following conversion formula: C = F-32 / 1.8",
     fileNames: ["index.html"],
   },
-  
   {
     id: 3,
     description:
       "Write a program to input time in hours(1hrs to 24hrs) and input am or pm as string, Display welcome message for the user.",
+    fileNames: ["index.html"],
+  },
+  {
+    id: 4,
+    description:
+      "Write a program to input three subjects marks and calculate average marks and display grade according to average marks",
     fileNames: ["index.html"],
   },
 ];
